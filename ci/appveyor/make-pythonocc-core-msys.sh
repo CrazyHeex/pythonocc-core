900c4b18243ef429e0b3e5f6f1de8893357a6313
tpaviot <tpaviot@gmail.com>
2016-05-12 15:52:48 +0200

    download oce binaries

--- ci/appveyor/make-pythonocc-core-msys.sh
+++ ci/appveyor/make-pythonocc-core-msys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cd `dirname "$0"`/..
+cd /c/projects/pythonocc-core
 if [ "$ARCH" = Win32 ]; then
   PATH=$PATH:/c/MinGW/bin
 elif [ "$ARCH" = i686 ]; then 
@@ -22,15 +22,38 @@ else
   7z x $f > /dev/null
   mv mingw64 /MinGW
 fi
+# download/extract oce-0.16.1
+f=OCE-0.16.1-Mingw32.zip
+if ! [ -e $f ]; then
+  echo "Downloading $f"
+  curl -LsSO https://github.com/tpaviot/oce/releases/download/OCE-0.16.1/$f
+fi
+echo "Extracting $f"
+7z x $f > /dev/null
+# download/extract sig3.0.8
+f=swigwin-3.0.8.zip
+if ! [ -e $f ]; then
+  echo "Downloading $f"
+  curl -LsSO https://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.8/$f
+fi
+echo "Extracting $f"
+7z x $f > /dev/null
+#
+echo "current directory"
+pwd
+ls C:/projects
+ls C:/projects/pythonocc-core/swigwin-3.0.8
 g++ -v
 # make pythonocc-core
 # test if file is present
-ls C:/projects/pythonocc-core/src/SWIG_files/wrapper
+#ls C:/projects/pythonocc-core/src/SWIG_files/wrapper
 cd /c/projects/pythonocc-core
 mkdir cmake-build
 cd cmake-build
-cmake -DOCE_INCLUDE_PATH=$PYTHON\\pkgs\\oce-0.16.1-vc9_5\\Library\\include\\oce \
-      -DOCE_LIB_PATH=$PYTHON\\pkgs\\oce-0.16.1-vc9_5\\Library\\lib \
+cmake -DOCE_INCLUDE_PATH=C\\projects\\pythonocc-core\\OCE-0.16.1-Mingw32\\include\\oce \
+      -DOCE_LIB_PATH=C\\projects\\pythonocc-core\\OCE-0.16.1-Mingw32\\Win32\\lib \
+      -DSWIG_EXECUTABLE=C:/projects/pythonocc-core/swigwin-3.0.8/swig.exe \
+      -DSWIG_DIR=C:/projects/pythonocc-core/swigwin-3.0.8/Lib \
       -G'MSYS Makefiles' ..
 mingw32-make -j4
 mingw32-make install
